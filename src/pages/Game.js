68c2acb938eceab15639b914412a4067a7d34dec
leandro-miranda <leandromiranda2010@gmail.com>
2022-08-09 14:33:25 -0300

    requisitos 7 e 8 feitos

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../component/Header';
 import { fetchTrivia } from '../services/fetchApi';
+import '../style.component/game.css';
+import Timer from '../component/Timer';
 
 // import { fetchGravatar } from '../services/fetchApi';
 class Game extends Component {
   state = {
     returnQuestions: [],
     returnCode: 0,
+    disabled: false,
   }
 
   componentDidMount() {
@@ -23,9 +26,28 @@ class Game extends Component {
     });
   }
 
+  // questionsChecked = (question, index) => {
+  //   const { returnQuestions } = this.props;
+  //   if (question === returnQuestions[0].correct_answer) return 'correct-answer';
+  //   return `wrong-answer-${!index ? 0 : index - 1}`;
+  // }
+
+  // questionsColors = (question) => {
+  //   const { returnQuestions } = this.props;
+  //   if (question === returnQuestions[0].correct_answer) return 'correct-answer';
+  //   return 'wrong-answer';
+  // }
+
+  btnDisabled = () => {
+    this.setState((prevState) => ({
+      disabled: !prevState.prevState,
+    }));
+  }
+
   render() {
-    const { returnQuestions, returnCode } = this.state;
+    const { returnQuestions, returnCode, disabled } = this.state;
     const { history } = this.props;
+
     let newArray = [];
 
     if (returnQuestions.length > 0) {
@@ -33,6 +55,8 @@ class Game extends Component {
         <button
           key={ index }
           type="button"
+          disabled={ disabled }
+          className="wrong-answer"
           data-testid={ `wrong-answer-${index}` }
           onClick={ () => {} }
         >
@@ -42,6 +66,8 @@ class Game extends Component {
       (
         <button
           type="button"
+          disabled={ disabled }
+          className="correct-answer"
           data-testid="correct-answer"
           key="correct"
           onClick={ () => {} }
@@ -71,7 +97,12 @@ class Game extends Component {
               {returnQuestions.length > 0
           && returnQuestions[0].question}
             </h2>
-            <div data-testid="answer-options">
+            <Timer
+              btnDisabled={ () => this.btnDisabled() }
+            />
+            <div
+              data-testid="answer-options"
+            >
               {newArray}
             </div>
           </div>)}
@@ -84,6 +115,7 @@ Game.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  // secondsTime: PropTypes.func.isRequired,
 };
 
 export default Game;
